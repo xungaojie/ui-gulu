1f111b626cf6f0eb7e421a877b386a2ecb6be618
jie-uncle <6030804@qq.com>
2019-04-03 11:13:15 +0800

    dist cache

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 一个vue组件
+使用本框架前   请在css中设置box-sizing：border-box
---
+++ src/button.vue
@@ -0,0 +1,31 @@
+<template>
+	<button class="g-button">
+		<slot></slot>
+	</button>
+</template>
+
+<script>
+	export default {
+	}
+</script>
+
+<style lang="stylus" scoped>
+.g-button{
+		font-size: var(--font-size);
+		height: var(--button-height);
+		background: var(--button-bg);
+		border-radius: var(--border-radius);
+		color: var(--color);
+		border:1px solid var(--border-color);
+		padding:0 1em;
+		&:hover{
+				border-color: var(--border-color-hover)
+		}
+		&:active{
+				background-color: var(--button-active-bg)
+		}
+		&:focus{
+				outline: none
+		}
+}
+</style>
